02ef0a7edb2df514ebe23e6d1bac1606184fca37
Joan TERIIHOANIA <joprocorp@gmail.com>
2022-03-11 11:32:43 +0100

    Initial commit

---
+++ src/fr/joanteriihoania/algoavance_grille/IFactory.java
@@ -0,0 +1,6 @@
+package fr.joanteriihoania.algoavance_grille;
+
+public interface IFactory{
+    //utile pour le départ du HillClimbing, dans lequel on veut partir d'un élément au hasard
+    IElemHC getRandomSol();
+}
---
+++ src/fr/joanteriihoania/algoavance_grille/Solution.java
@@ -0,0 +1,15 @@
+package fr.joanteriihoania.algoavance_grille;
+
+import java.util.ArrayList;
+
+
+public class Solution extends ArrayList<Coord> {
+    public Solution(Coord c){
+        this.add(c);
+    }
+    public Solution(){
+
+    }
+
+
+}
