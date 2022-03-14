8dc13fd049270275b68710d5743c24f5177d5edd
joan.teriihoania <joprocorp@gmail.com>
2022-03-14 12:08:39 +0100

    Added prog dyn to algoFPT1

--- TP-TERIIHOANIA-FERNIQUE/Algos.java
+++ TP-TERIIHOANIA-FERNIQUE/Algos.java
@@ -115,25 +115,26 @@ public class Algos {
     public static Solution algoFPT1DP(InstanceDec id, HashMap<InstanceDec, Solution> table) {
         //même spécification que algoFPT1, si ce n'est que
         // - si table.containsKey(id), alors id a déjà été calculée, et on se contente de retourner table.get(id)
+        if (!table.containsKey(id)) {
+            Solution sol = algoFPT1(id);
+            table.put(id, sol);
+        }
         // - sinon, alors on doit calculer la solution s pour id, la ranger dans la table (table.put(id,res)), et la retourner
+        return table.get(id);
 
         //Remarques
         // - ne doit pas modifier l'instance id en param (mais va modifier la table bien sûr)
         // - même si le branchement est le même que dans algoFPT1, ne faites PAS appel à algoFPT1 (et donc il y aura de la duplication de code)
-
-
-        //à compléter
-        return null;
     }
 
 
     public static Solution algoFPT1DPClient(InstanceDec id){
+        HashMap<InstanceDec, Solution> table = new HashMap<>();
         //si il est possible de collecter >= id.c pièces dans id.i, alors retourne une Solution de valeur >= c, sinon retourne null
         //doit faire appel à algoFPT1DP
 
         //à completer
-        return null;
-
+        return algoFPT1DP(id, table);
     }
 
 
