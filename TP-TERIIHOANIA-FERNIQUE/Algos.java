cfd4e17e1369e1946ffd2bacfc49272b720ff401
Joprocorp <joprocorp@gmail.com>
2022-03-23 12:04:39 +0100

    Adding programming dynamic optimization

--- TP-TERIIHOANIA-FERNIQUE/Algos.java
+++ TP-TERIIHOANIA-FERNIQUE/Algos.java
@@ -61,7 +61,6 @@ public class Algos {
             } else {
                 return null;
             }
-
         }
         nextInstanceDec.i.setK(id.i.getK() - 1);
 
@@ -106,7 +105,88 @@ public class Algos {
             }
         }
         return null;
+    }
 
+    /**
+     * @param id     nouvelle instance qui a éte parcouru par le chemin
+     * @param chemin chemin déja parcouru
+     * @return la solution qui satisfait c ou sinon null si k==0
+     */
+    private static Solution rechercheOptFPTDP(InstanceDec id, Solution chemin, HashMap<InstanceDec, Solution> table) {
+        if(table.containsKey(id)){
+            return table.get(id);
+        }
+
+        Solution nextChemin = new Solution();
+
+        nextChemin.addAll(chemin);
+        nextChemin.add(id.i.getStartingP());
+
+        InstanceDec nextInstanceDec = new InstanceDec(new Instance(id.i), id.c);
+
+        if (nextInstanceDec.i.piecePresente(nextInstanceDec.i.getStartingP())) {
+            nextInstanceDec.c--;
+            nextInstanceDec.i.retirerPiece(nextInstanceDec.i.getStartingP());
+        }
+
+        //cas de base
+        if (nextInstanceDec.i.getK() == 0) {
+            if (nextInstanceDec.c <= 0) {
+                table.put(id, nextChemin);
+            } else {
+                table.put(id, null);
+            }
+            return table.get(id);
+        }
+        nextInstanceDec.i.setK(id.i.getK() - 1);
+
+        //Recupérer les coord des case dans les 4 directions
+        Coord left = new Coord(id.i.getStartingP().getL(), id.i.getStartingP().getC() - 1);
+        Coord up = new Coord(id.i.getStartingP().getL() - 1, id.i.getStartingP().getC());
+        Coord right = new Coord(id.i.getStartingP().getL(), id.i.getStartingP().getC() + 1);
+        Coord down = new Coord(id.i.getStartingP().getL() + 1, id.i.getStartingP().getC());
+
+        Solution leftSol;
+        Solution upSol;
+        Solution downSol;
+        Solution rightSol;
+        nextInstanceDec.i.setK(id.i.getK() - 1);
+
+        if (left.estDansPlateau(nextInstanceDec.i.getNbL(), nextInstanceDec.i.getNbC())) {
+            nextInstanceDec.i.setStartingP(left);
+            leftSol = rechercheOptFPTDP(nextInstanceDec, nextChemin, table);
+            if (leftSol != null) {
+                table.put(id, leftSol);
+                return table.get(id);
+            }
+        }
+        if (up.estDansPlateau(nextInstanceDec.i.getNbL(), nextInstanceDec.i.getNbC())) {
+            nextInstanceDec.i.setStartingP(up);
+            upSol = rechercheOptFPTDP(nextInstanceDec, nextChemin, table);
+            if (upSol != null) {
+                table.put(id, upSol);
+                return table.get(id);
+            }
+        }
+        if (right.estDansPlateau(nextInstanceDec.i.getNbL(), nextInstanceDec.i.getNbC())) {
+            nextInstanceDec.i.setStartingP(right);
+            rightSol = rechercheOptFPTDP(nextInstanceDec, nextChemin, table);
+            if (rightSol != null) {
+                table.put(id, rightSol);
+                return table.get(id);
+            }
+        }
+        if (down.estDansPlateau(nextInstanceDec.i.getNbL(), nextInstanceDec.i.getNbC())) {
+            nextInstanceDec.i.setStartingP(down);
+            downSol = rechercheOptFPTDP(nextInstanceDec, nextChemin, table);
+            if (downSol != null) {
+                table.put(id, downSol);
+                return table.get(id);
+            }
+        }
+
+        table.put(id, null);
+        return null;
     }
 
 
@@ -115,16 +195,22 @@ public class Algos {
     public static Solution algoFPT1DP(InstanceDec id, HashMap<InstanceDec, Solution> table) {
         //même spécification que algoFPT1, si ce n'est que
         // - si table.containsKey(id), alors id a déjà été calculée, et on se contente de retourner table.get(id)
-        if (!table.containsKey(id)) {
-            Solution sol = algoFPT1(id);
-            table.put(id, sol);
-        }
         // - sinon, alors on doit calculer la solution s pour id, la ranger dans la table (table.put(id,res)), et la retourner
-        return table.get(id);
 
         //Remarques
         // - ne doit pas modifier l'instance id en param (mais va modifier la table bien sûr)
         // - même si le branchement est le même que dans algoFPT1, ne faites PAS appel à algoFPT1 (et donc il y aura de la duplication de code)
+        if (table.containsKey(id)) {
+            return table.get(id);
+        }
+
+        if (id.c == 0) {
+            table.put(id, new Solution(id.i.getStartingP()));
+            return table.get(id);
+        }
+
+        table.put(id, rechercheOptFPTDP(id, new Solution(), table));
+        return table.get(id);
     }
 
 
